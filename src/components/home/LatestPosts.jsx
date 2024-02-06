63a13c5de9fbdeed7ae5bf3dbc0d04482345c1cb
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-05 19:58:29 -0800

    articals-sec-style

--- src/components/home/LatestPosts.jsx
+++ src/components/home/LatestPosts.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const LatestPosts = () => {
+  const navigate = useNavigate();
   return (
-    <div>LatestPosts</div>
-  )
-}
+    <section className="py-5">
+      <Container>
+        <h2 className="text-center mb-5"> Latest Articles</h2>
 
-export default LatestPosts
+        <Row xs="1" md="2" lg="4" className="g-4">
+          {new Array(4).fill("x").map((_, ix) => (
+            <Col key={ix}>
+              <div> article </div>
+            </Col>
+          ))}
+        </Row>
+
+        <div className="mt-4 text-center">
+          <Button
+            variant="outline-dark"
+            className="px-5"
+            onClick={() => navigate("/blog")}
+          >
+            {" "}
+            see all
+          </Button>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default LatestPosts;
