e4461549a41bc466339f45bf46e7a817517e87fe
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-02-06 03:07:11 -0800

    getData

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { PostsContext } from "../../context/PostsContext";
 
 const MainBlog = () => {
-  return <div>MainBlog</div>;
+  const { fetch } = useContext(PostsContext);
+  const isMount = useRef(false);
+
+  useEffect(() => {
+    if (!isMount.current) {
+      fetch();
+      isMount.current = true;
+    }
+  }, []);
+
+  return (
+    <section className="py-5">
+      <Container>
+        <h2 className="text-center mb-5"> Latest Posts</h2>
+
+        <Row xs="1" md="2" lg="4" className="g-4">
+          <Col> post</Col>
+          <Col> post</Col>
+          <Col> post</Col>
+          <Col> post</Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default MainBlog;
--- src/context/PostsContext.js
+++ src/context/PostsContext.js
@@ -1,13 +1,22 @@
 import { createContext, useCallback } from "react";
+import useFetchCol from "../hooks/useFetchCol";
 
-export const PostsContext = createContext();
+
+export const PostsContext = createContext()
 
 export const PostsProvider = ({ children }) => {
+
+  const { getData, data } = useFetchCol('posts')
+
   const fetch = useCallback(() => {
-    return (
-      <PostsContext.Provider value={{}}>
-        {children}
-      </PostsContext.Provider>
-    );
-  });
-};
+
+    if (!data) {
+      getData()
+    }
+  }, [data, getData])
+
+
+  return <PostsContext.Provider value={{ fetch }}>
+    {children}
+  </PostsContext.Provider>
+}
